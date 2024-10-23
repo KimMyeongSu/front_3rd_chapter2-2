19a5e4f303e5a885e92bdf6f6133b5c365154c10
KimMyeongSu <kms930508@gmail.com>
2024-10-23 17:12:51 +0900

    basic 테스트코드 기능 완료, 관심사,계층구조 분할 작업 진행예정

--- src/refactoring/hooks/utils/cartUtils.ts
+++ src/refactoring/hooks/utils/cartUtils.ts
@@ -1,28 +1,72 @@
 import { CartItem, Coupon } from "../../../types";
 
-export const calculateItemTotal = (item: CartItem) => {
-  return item.product.price * item.quantity;
+export const calculateItemTotal = (item: CartItem): number => {
+  return (
+    item.product.price * item.quantity * (1 - getMaxApplicableDiscount(item))
+  );
 };
 
-export const getMaxApplicableDiscount = (item: CartItem) => {
-  return 0;
+export const getMaxApplicableDiscount = ({
+  product,
+  quantity,
+}: CartItem): number => {
+  const { discounts } = product;
+  let appliedDiscount = 0;
+
+  for (const discount of discounts) {
+    if (quantity >= discount.quantity) {
+      appliedDiscount = Math.max(appliedDiscount, discount.rate);
+    }
+  }
+
+  return appliedDiscount;
 };
 
 export const calculateCartTotal = (
-  cart: CartItem[],
+  items: CartItem[],
   selectedCoupon: Coupon | null
-) => {
+): {
+  totalBeforeDiscount: number;
+  totalAfterDiscount: number;
+  totalDiscount: number;
+} => {
+  const totalBeforeDiscount = items.reduce(
+    (sum, item) => sum + item.quantity * item.product.price,
+    0
+  );
+
+  const totalItem = items.reduce(
+    (sum, item) => sum + calculateItemTotal(item),
+    0
+  );
+
+  const totalAfterDiscount = !selectedCoupon
+    ? totalItem
+    : selectedCoupon.discountType === "amount"
+    ? totalItem - selectedCoupon.discountValue
+    : (totalItem * (100 - selectedCoupon.discountValue)) / 100;
+
+  const totalDiscount = totalBeforeDiscount - totalAfterDiscount;
+
   return {
-    totalBeforeDiscount: 0,
-    totalAfterDiscount: 0,
-    totalDiscount: 0,
+    totalBeforeDiscount,
+    totalAfterDiscount,
+    totalDiscount,
   };
 };
 
 export const updateCartItemQuantity = (
-  cart: CartItem[],
+  items: CartItem[],
   productId: string,
   newQuantity: number
 ): CartItem[] => {
-  return [];
+  if (newQuantity === 0) {
+    return items.filter((item) => item.product.id !== productId);
+  }
+
+  return items.map((item) =>
+    item.product.id === productId
+      ? { ...item, quantity: Math.min(item.product.stock, newQuantity) }
+      : item
+  );
 };
